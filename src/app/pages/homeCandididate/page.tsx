70a067b27d20e6b2fe2a3fe1fa890145dc488792
inclusivee <dev@mommytech.com.br>
2024-01-12 14:29:17 -0300

    Fix: Bug in build companyhome

--- src/app/pages/homeCandididate/page.tsx
+++ src/app/pages/homeCandididate/page.tsx
@@ -1,5 +1,5 @@
 export default function CandidateHome() {
   return (
-    <></>
+    <><h1>Hi world!</h1></>
   )
 }
--- src/app/pages/homeCompany/page.tsx
+++ src/app/pages/homeCompany/page.tsx
@@ -1,6 +1,6 @@
 export default function CompanyHome() {
     return (
-      <></>
+      <> <h1>Hi world!</h1></>
     )
   }
   
